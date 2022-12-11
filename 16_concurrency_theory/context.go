4171efaaa925582a60169927c6796addbca13aea
cortez720 <ivankalinin720@mail.ru>
2022-12-12 00:25:33 +0500

    :hammer: context

---
+++ 16_concurrency_theory/context.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type impl int
+
+type Position struct {
+	Lat float32
+	Lng float32
+}
+
+type publisher interface {
+	Publish(ctx context.Context, position Position) error
+}
+
+type publishHandler struct {
+	pub publisher
+}
+
+func (h publishHandler) publishPosition(position Position) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 9*time.Millisecond)
+	defer cancel() // Safeguard to don't leave retained objects in memory
+	return h.pub.Publish(ctx, position)
+}
+
+func (impl) Publish(ctx context.Context, position Position) error {
+	deadline, _ := ctx.Deadline()
+
+	time.Sleep(time.Millisecond * 10)
+
+	if deadline.Before(time.Now()) {
+		return context.Canceled
+	}
+
+	fmt.Println(position)
+
+	return nil
+}
+
+func main() {
+	var p impl
+	ph := publishHandler{p}
+	err := ph.publishPosition(Position{13.5542, 2123.123213})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
