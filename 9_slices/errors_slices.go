665f6daf2d9e552de3e44024c297d8cd24c32ca7
cortez720 <93537782+cortez720@users.noreply.github.com>
2023-01-31 11:44:39 +0500

    Update errors_slices.go

--- 9_slices/errors_slices.go
+++ 9_slices/errors_slices.go
@@ -18,6 +18,6 @@ func main() {
 	me := MultiErrorSl{errors: make([]error, 0, 1)} // Allocate memory, thats why we append element in existing array under slice
 	// If there are no memory, new array will be allocated in a copy of struct and we will not have error there.
 	err := errors.New("error 1")
-	me.Add(err)
+	me.Add(err) // Copy with same array under slice, but not samae len value in slice struct
 	spew.Dump(me.errors[:1])
 }
